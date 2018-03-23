6717d1c5cf2293e7a9dfbe73248699b2c567ab56
League <league@WTS9.attlocal.net>
2018-03-22 19:07:47 -0700

    Module 0

---
+++ src/Knock_Knock.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Knock_Knock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Knock knock!");
+	JOptionPane.showMessageDialog(null, "Who's there?");
+	JOptionPane.showMessageDialog(null, "Owls say!");
+	JOptionPane.showMessageDialog(null, "Owls say who?");
+	JOptionPane.showMessageDialog(null, "Owls Do!");
+}
+}
---
+++ src/MorningZombie.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Wake up");
+	JOptionPane.showMessageDialog(null, "dress");
+	JOptionPane.showMessageDialog(null, "brush and wash");
+	JOptionPane.showMessageDialog(null, "pack up");
+	JOptionPane.showMessageDialog(null, "eat");
+	JOptionPane.showMessageDialog(null, "leave for school");
+}
+}
